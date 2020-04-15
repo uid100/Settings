2926eab8262884f293b835e150ffbb125b8c831a
Dan Sullivan <daniel.f.sullivan@gmail.com>
2020-04-14 21:49:31 -0700

    Add project files.

---
+++ Settings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace Settings
+{
+    class Program
+    {
+        static void ReadConfiguration()
+        {
+            var appConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = appConfig.Build();
+
+            string[] items = configuration.GetSection("Pizza:size").Get<List<string>>();
+            foreach (var s in items) Console.WriteLine(s);
+
+        }
+
+        static void Main(string[] args)
+        {
+            ReadConfiguration();
+        }
+    }
+}
